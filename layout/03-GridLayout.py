9849a5ca49a31b77d5b20059e8d047966290daca
JinseongHwang <eddy5360@naver.com>
2021-08-07 13:27:05 +0900

    feat: add QComboBox

--- layout/03-GridLayout.py
+++ layout/03-GridLayout.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import (
-    QApplication, QWidget, QGridLayout, QLabel, QLineEdit, QTextEdit)
+    QApplication, QWidget, QGridLayout, QLabel, QLineEdit, QTextEdit, QComboBox
+)
 
 
 class MyApp(QWidget):
@@ -13,16 +14,21 @@ class MyApp(QWidget):
         grid = QGridLayout()
         self.setLayout(grid)
 
+        comboBox = QComboBox()
+        comboBox.addItems(['1', '2', '3', '4', '5'])
+
         #####
         # addWidget(Object, Row, Column)
         #####
         grid.addWidget(QLabel('Title:'), 0, 0)
         grid.addWidget(QLabel('Author:'), 1, 0)
-        grid.addWidget(QLabel('Review:'), 2, 0)
+        grid.addWidget(QLabel('Rate:'), 2, 0)
+        grid.addWidget(QLabel('Review:'), 3, 0)
 
         grid.addWidget(QLineEdit(), 0, 1)  # Line editor
         grid.addWidget(QLineEdit(), 1, 1)  # Line editor
-        grid.addWidget(QTextEdit(), 2, 1)  # Text editor
+        grid.addWidget(comboBox, 2, 1)
+        grid.addWidget(QTextEdit(), 3, 1)  # Text editor
 
         self.setWindowTitle('QGridLayout')
         self.setGeometry(300, 300, 300, 200)
